a86bda325a3f91941e186aeddb3ba291cef0d5d8
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-09 00:53:52 +0530

    ADDED: Test case to check for valid and invalid mobile numbers

--- lib/src/test/resources/com/brigelabz/uservalidator/UserValidatorTest.java
+++ lib/src/test/resources/com/brigelabz/uservalidator/UserValidatorTest.java
@@ -5,76 +5,93 @@ import org.junit.Test;
 
 public class UserValidatorTest {
 
-    @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue()
-    {
-
-        UserValidator userValidator = new UserValidator();
-        boolean isValid=userValidator.validateName("Emma");
-        Assert.assertTrue(isValid);
-
-    }
-    @Test
-	public void givenFirstName_WhenShort_ShouldReturnFalse() 
-	{
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
 
 		UserValidator userValidator = new UserValidator();
-		boolean isValid=userValidator.validateName("Em");
-		Assert.assertFalse(isValid);
-
-	}
-    @Test
-	public void givenFirstName_WhenFirstLetterNotCapital_ShouldReturnFalse() 
-	{
-
-		UserValidator userValidator = new UserValidator();
-		boolean isValid=userValidator.validateName("emma");
-		Assert.assertFalse(isValid);
-
-	}
-    @Test
-	public void givenLastName_WhenProper_ShouldReturnTrue() 
-	{
-
-		UserValidator userValidator = new UserValidator();
-		boolean isValid=userValidator.validateName("Wilson");
+		boolean isValid = userValidator.validateName("Emma");
 		Assert.assertTrue(isValid);
 
 	}
+
 	@Test
-	public void givenLastName_WhenShort_ShouldReturnFalse() 
-	{
+	public void givenFirstName_WhenShort_ShouldReturnFalse() {
 
 		UserValidator userValidator = new UserValidator();
-		boolean isValid=userValidator.validateName("Wi");
+		boolean isValid = userValidator.validateName("Em");
 		Assert.assertFalse(isValid);
 
 	}
-	 @Test
-		public void givenLastName_WhenFirstLetterNotCapital_ShouldReturnFalse() 
-		{
 
-			UserValidator userValidator = new UserValidator();
-			boolean isValid=userValidator.validateName("wilson");
-			Assert.assertFalse(isValid);
+	@Test
+	public void givenFirstName_WhenFirstLetterNotCapital_ShouldReturnFalse() {
 
-		}
-	 @Test
-		public void givenEmail_WhenValid_ShouldReturnTrue() 
-		{
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateName("emma");
+		Assert.assertFalse(isValid);
 
-			UserValidator userValidator = new UserValidator();
-			boolean isValid=userValidator.validateEMail("abc@gmail.com");
-			Assert.assertTrue(isValid);
+	}
 
-		}
-	 @Test
-		public void givenEmail_WhenInValid_ShouldReturnFalse() 
-		{
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
 
-			UserValidator userValidator = new UserValidator();
-			boolean isValid=userValidator.validateEMail("abcgmail.com");
-			Assert.assertFalse(isValid);
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateName("Wilson");
+		Assert.assertTrue(isValid);
 
-		}
+	}
+
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() {
+
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateName("Wi");
+		Assert.assertFalse(isValid);
+
+	}
+
+	@Test
+	public void givenLastName_WhenFirstLetterNotCapital_ShouldReturnFalse() {
+
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateName("wilson");
+		Assert.assertFalse(isValid);
+
+	}
+
+	@Test
+	public void givenEmail_WhenValid_ShouldReturnTrue() {
+
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateEMail("abc@gmail.com");
+		Assert.assertTrue(isValid);
+
+	}
+
+	@Test
+	public void givenEmail_WhenInValid_ShouldReturnFalse() {
+
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateEMail("abcgmail.com");
+		Assert.assertFalse(isValid);
+
+	}
+
+	@Test
+	public void givenPhoneNumber_WhenValid_ShouldReturnTrue() {
+
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateMobileNumber("91 9191123498");
+		Assert.assertTrue(isValid);
+
+	}
+
+	@Test
+	public void givenPhoneNumber_WhenInValid_ShouldReturnFalse() {
+
+		UserValidator userValidator = new UserValidator();
+		boolean isValid = userValidator.validateMobileNumber("123456789");
+		Assert.assertFalse(isValid);
+
+	}
 }
