a292831fe8db070b91ba24f41a92e46d593e24cc
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-09 00:46:15 +0530

    ADDED: Test case to return false for short last name

--- lib/src/test/resources/com/brigelabz/uservalidator/UserValidatorTest.java
+++ lib/src/test/resources/com/brigelabz/uservalidator/UserValidatorTest.java
@@ -41,4 +41,13 @@ public class UserValidatorTest {
 		Assert.assertTrue(isValid);
 
 	}
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() 
+	{
+
+		UserValidator userValidator = new UserValidator();
+		boolean isValid=userValidator.validateName("Em");
+		Assert.assertFalse(isValid);
+
+	}
 }
